3257d9e7ea873add6608808f186a01aa6606b70d
Alessio Ilardi <alessio.ilardi@gmail.com>
2023-03-16 10:36:52 +0100

    👌 IMPROVE: add CSS classes to Input and Select elements

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,19 +1,22 @@
+import React from "react";
 import { useField } from "formik";
-import React, { memo } from "react";
 import { InputItem } from "../Form/config";
 
 type Props = {
   label?: string;
 } & InputItem;
 
-const Input = ({ label, id, ...props }: Props) => {
+const Input = ({ label, id, children, ...props }: Props) => {
   const [field, meta] = useField({ ...props, name: id });
   return (
-    <>
+    <div className="input-item">
       {label && <label htmlFor={id}>{label}</label>}
+      {/* {children ? children.map(({}) =>)} */}
       <input className="text-input" {...field} {...props} />
-      {meta.touched && meta.error && <div className="error">{meta.error}</div>}
-    </>
+      {meta.touched && meta.error && (
+        <div className="validation-error">{meta.error}</div>
+      )}
+    </div>
   );
 };
 
--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,32 +1,29 @@
 import React from "react";
 import { useField } from "formik";
-import { InputItem } from "../Form/config";
-
-export type SelectOption = {
-  label: string;
-  value: string;
-};
+import { InputItem, SelectOption } from "../Form/types";
 
 type Props = {
   label?: string;
   options?: SelectOption[];
 } & InputItem;
 
-function Select({ label, options, type, id, ...props }: Props) {
+function Select({ label, options = [], type, id, children, ...props }: Props) {
   const [field, meta] = useField({ ...props, name: id });
 
   return (
-    <>
+    <div className="input-item">
       {label && <label htmlFor={id}>{label}</label>}
       <select {...field} {...props}>
-        {options?.map(({ label, value }) => (
+        {options.map(({ label, value }) => (
           <option value={value} key={value}>
             {label}
           </option>
         ))}
       </select>
-      {meta.touched && meta.error && <div className="error">{meta.error}</div>}
-    </>
+      {meta.touched && meta.error && (
+        <div className="validation-error">{meta.error}</div>
+      )}
+    </div>
   );
 }
 
