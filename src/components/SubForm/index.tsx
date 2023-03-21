2c09277d133ecbf1481b6f86de39136b2013684d
Alessio Ilardi <alessio.ilardi@gmail.com>
2023-03-21 09:45:49 +0100

    👌 IMPROVE: create button to add new children to subforms

--- src/components/SubForm/index.tsx
+++ src/components/SubForm/index.tsx
@@ -1,17 +1,31 @@
 import React from "react";
-import { inputMap } from "../Form";
+import { formMap } from "../Form";
 import { InputItem } from "../Form/types";
 
 type Props = {
-  inputs?: InputItem[];
+  fields?: InputItem[];
   label?: string;
-};
+  onAddField: (id: string, fields: InputItem[]) => void;
+} & Omit<InputItem, "type">;
 
-function SubForm({ inputs = [], label }: Props) {
+function SubForm({ fields = [], id, label, multiple, onAddField }: Props) {
+  const handleClick = () => {
+    return onAddField(id, fields);
+  };
+
   return (
     <div className="subform">
       {label && <p className="subform-label">{label}</p>}
-      {inputMap(inputs)}
+      {formMap(fields, id)}
+      {multiple && (
+        <button
+          type="button"
+          className="add-input-button"
+          onClick={handleClick}
+        >
+          Add
+        </button>
+      )}
     </div>
   );
 }
